a1ded708346803d4a4f4b88425f9b9b55052360a
Jahanzaib <129555905+Jahanzaibop@users.noreply.github.com>
2025-02-04 17:37:49 +0500

    Updated MongoDB connection and backend improvements

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+export const connectDB = async () => {
+    try {
+        const dbURI = 'mongodb+srv://user2000:test1234@cluster22.bvyhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster22';
+        
+        mongoose.set('debug', true);  // Enable mongoose debugging
 
+        await mongoose.connect(dbURI, {
+           
+        });
 
-
-export const connectDB = async() =>{
-
-    try{
-        await mongoose.connect('mongodb://localhost:27017/moviedb');
-        console.log(`connected to the mongodb ${mongoose.connection.host}`);
-    }
-
-    catch(err){
-        console.error(err);
+        console.log('Connected to MongoDB Atlas');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
         process.exit(1);
     }
-}
+};
